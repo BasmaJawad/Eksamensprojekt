3361ef6a2423b3a15b382e88d8d213bbee77169a
albe194e <albe194e@stud.kea.dk>
2022-11-22 14:38:56 +0100

    DCM

--- src/main/java/com/example/eksamensprojekt/Misc/DCM.java
+++ src/main/java/com/example/eksamensprojekt/Misc/DCM.java
@@ -1,7 +1,30 @@
 package com.example.eksamensprojekt.Misc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DCM {
 
+    private static String hostname, username, password;
 
+    private static Connection conn = createConnection();
 
+    private static Connection createConnection(){
+
+        hostname = System.getenv("hostname");
+        username = System.getenv("username");
+        password = System.getenv("password");
+
+        try {
+            conn = DriverManager.getConnection(hostname,username,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conn;
+    }
+    public static Connection getConn(){
+        return conn;
+    }
 }
