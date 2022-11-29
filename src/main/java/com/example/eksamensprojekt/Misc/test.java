e270de4c27816174b178d6cb6c7371f6aadcb962
wrsame <jawa0262@stud.kea.dk>
2022-11-29 11:51:30 +0100

    homepage

--- src/main/java/com/example/eksamensprojekt/Misc/test.java
+++ src/main/java/com/example/eksamensprojekt/Misc/test.java
@@ -1,16 +1,16 @@
 package com.example.eksamensprojekt.Misc;
 
+import com.example.eksamensprojekt.Model.Contract;
 import com.example.eksamensprojekt.Repository.UsersRepository;
+import com.example.eksamensprojekt.Service.IncidentsService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class test {
 
     public static void main(String[] args) {
 
-
-
-        System.out.println(LocalDate.now());
 
     }
 }
